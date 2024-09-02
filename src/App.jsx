6477e13fd42d684e245fccd9c7222746e452a482
edpau <edpau@me.com>
2024-09-03 00:50:36 +0100

    feat: update App component to include Card component and apply Tailwind CSS classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Card from "./components/Card";
+
 const App = () => {
   return (
-    <div>
-      <div>APP</div>
+    <div className="w-full overflow-hidden bg-brand-primary">
+      <main className="flex min-h-screen w-screen justify-center items-center">
+        <Card />
+      </main>
     </div>
   );
 };
