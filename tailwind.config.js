fdcd90e1f8b1a0a79266663a47551792413a76f3
edpau <edpau@me.com>
2024-09-03 00:47:58 +0100

    chore: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,23 @@ export default {
       backgroundColor: {
         "brand-primary": "hsl(30, 38%, 92%)",
         "brand-secondary": "hsl(158, 36%, 37%)",
+        "brand-secondary-hover": "hsl(157.9,42.2%,17.6%)",
         "neutral-primary": "hsl(0, 0%, 100%)",
       },
       textColor: {
         "brand-primary": "hsl(158, 36%, 37%)",
         "neutral-primary": "hsl(228, 12%, 48%)",
         "neutral-secondary": "hsl(212, 21%, 14%)",
-        "onbrand": "hsl(0, 0%, 100%)",
+        "onbrand-primary": "hsl(0, 0%, 100%)",
       },
       fontSize: {
         paragraph: "14px",
       },
       fontFamily: {
         heading: ["Fraunces", "serif"],
+        subHeading: ["Montserrat", "sans-serif"],
         body: ["Montserrat", "sans-serif"],
+        priceTag: ["Fraunces", "serif"],
       },
       fontWeightCustom: {
         regular: 500,
