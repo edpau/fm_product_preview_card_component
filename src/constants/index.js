993f162a053e2a22601d841acebc836681f6b8e7
edpau <edpau@me.com>
2024-09-04 03:05:52 +0100

    fix: add missing dollar sign

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,8 +7,8 @@ export const products = [
         category: "Perfume",
         productName: "Gabrielle Essence Eau De Parfum",
         description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
-        discountedPrice: "149.99",
-        originalPrice: "169.99",
+        discountedPrice: "$149.99",
+        originalPrice: "$169.99",
         image: {
             desktop: perfumeDesktopImg,
             mobile: perfumeMobileImg,
