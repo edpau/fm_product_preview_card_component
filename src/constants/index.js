08a6c92121af435412a86905d6ce958a215c5eac
edpau <edpau@me.com>
2024-09-04 01:15:59 +0100

    feat: add product data

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+import perfumeMobileImg from "../assets/images/image-product-perfume-mobile.jpg"
+import perfumeDesktopImg from "../assets/images/image-product-perfume-desktop.jpg"
+
+export const products = [
+    {
+        id: 1,
+        category: "Perfume",
+        productName: "Gabrielle Essence Eau De Parfum",
+        description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+        discountedPrice: "149.99",
+        originalPrice: "169.99",
+        image: {
+            desktop: perfumeDesktopImg,
+            mobile: perfumeMobileImg,
+        },
+        imgAlt: "Square glass bottle of Gabrielle Chanel perfume with a sleek gold cap, placed beside green leaves on a neutral surface.",
+    }
+]
