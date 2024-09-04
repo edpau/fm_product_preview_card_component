6b2623eea669935e8bade6f931a65fd033684cce
edpau <edpau@me.com>
2024-09-04 01:10:27 +0100

    feat: pass product prop to Card component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Card from "./components/Card";
+import { products } from "./constants/index";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden bg-brand-primary">
-      <main className="flex min-h-screen w-screen justify-center items-center">
-        <Card />
+      <main className="flex min-h-screen w-screen items-center justify-center">
+        {products.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
       </main>
     </div>
   );
