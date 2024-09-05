ef92b363ca3398c7a51c5a7d97c0a84b20ccb442
edpau <edpau@me.com>
2024-09-05 13:01:39 +0100

    refactor: change the background color to index.html body from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { products } from "./constants/index";
 
 const App = () => {
   return (
-    <div className="w-full overflow-hidden bg-brand-primary">
+    <div className="w-full overflow-hidden">
       <main className="flex min-h-screen w-screen items-center justify-center">
         {products.map((product) => (
           <Card key={product.id} product={product} />
