97ff7a0588fc8cdc68e8fd755736d161a90429ce
Ed <onikage116@gmail.com>
2024-09-10 02:24:36 +0100

    feat: add sr-only h1 for Product Review to improve accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { products } from "./constants/index";
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <h1 className="sr-only">Product Review</h1>
       <main className="flex min-h-screen w-screen items-center justify-center">
         {products.map((product) => (
           <Card key={product.id} product={product} />
